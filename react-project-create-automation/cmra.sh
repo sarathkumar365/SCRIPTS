c947df483774279a49fce21a68dbcf3bfd23db50
sarathkumar365 <sarathkumark.s365@gmail.com>
2022-07-21 13:38:57 +0530

    added storybook support

--- react-project-create-automation/cmra.sh
+++ react-project-create-automation/cmra.sh
@@ -3,6 +3,10 @@
 echo "Enter your project name"
 read project_name
 
+echo "Do you want to install Storybook (y/n) : "  
+read storybook_choice
+
+
 git clone https://github.com/sarathkumar365/My-React-Template.git
 
 mv My-React-Template $project_name
@@ -10,6 +14,15 @@ mv My-React-Template $project_name
 cd $project_name
 
 npm install
+
+if [ $storybook_choice == "y" ]
+then
+	echo "installing storybook"
+	npx -p @storybook/cli sb init
+
+else
+	echo "NO storybook"
+fi
 
 echo "run npm start to start your app in development mode"
 echo "run npm build to build your app"
